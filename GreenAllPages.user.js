a6cf0710f17e23e13738bdcd560a22ae54ffb0e9
Thomson <lilotom@gmail.com>
2015-01-24 23:07:41 +0800

    Update for stackoverflow
    Also change the load time of this script to document-start.

--- GreenAllPages.user.js
+++ GreenAllPages.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
-// @name	Change the background page of current web page to green
+// @name	Green page background
 // @namespace	http://www.TianshengTan.com
 // @description     This script should work for all pages without CSS. version 1.0
 // @match	http://*/*
 // @match https://*/*
-// @run-at      document-end
+// @run-at      document-start
 // ==/UserScript==
 
 // green current page, or insert other CSS element to DOM directly
 // This is used to be done in custom.css, but it is not supported from chrome 32/33
+// document-start works? I remembered it didn't work in the past
 
 // installation guide: drag this file to chrome://extensions page directly
 
@@ -18,9 +19,13 @@ var style = document.createElement('style');
 
 style.type = 'text/css';
 
-style.innerHTML = "html, body {background-color: #8ACA9A!important;}"
+if (document.URL.indexOf("http://stackoverflow.com") == 0) {
+    style.innerHTML = "html, body {background-color: #8ACA9A!important;} body #content{background-color: #8ACA9A!important;} body .tagged-interesting{background-color: #8ACA9A!important;}";
+}
+else {
+    style.innerHTML = "html, body {background-color: #8ACA9A!important;}";
+}
 
 document.getElementsByTagName("head")[0].appendChild( style );
-
 
 
