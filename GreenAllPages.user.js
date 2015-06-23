dd2cf2176f19da8ad1e8ca33d65f23ede331d89f
Thomson <lilotom@gmail.com>
2015-06-22 20:57:41 -0700

    Add Open Sans as default font style

--- GreenAllPages.user.js
+++ GreenAllPages.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name	Green page background
 // @namespace	http://www.TianshengTan.com
-// @description     This script should work for all pages without CSS. version 1.0
+// @description     This script should work for all pages without CSS. version 1.0, and it also set change font for all elements.
 // @match	http://*/*
 // @match https://*/*
 // @run-at      document-start
@@ -26,7 +26,7 @@ else if (document.URL.indexOf("https://vi.stackexchange.com") == 0) {
     style.innerHTML = "html, body {background-color: #8ACA9A!important;} body #content{background-color: #8ACA9A!important;} body .container{background-color: #8ACA9A!important;}";
 }
 else if (document.URL.indexOf("https://mail.google.com") == 0) {
-    style.innerHTML = "html, body .Bu{background-color: #8ACA9A!important;line-height: 1.1em;font-size:30px;font-family: Open Sans,Book Antiqua,verdana,serif;} body .hP{background-color: yellow} h3{background-color: gray}";
+    style.innerHTML = "html, body .Bu{background-color: #8ACA9A!important;line-height: 1.1em;font-size:30px;font-family: verdana,serif;} body .hP{background-color: yellow} h3{background-color: gray}";
 }
 else if (document.URL.indexOf("https://www.evernote.com") == 0) {
     style.innerHTML = "html, body {background-color: #8ACA9A!important;} body div.GAOIOH2DIGB{background: #9ACA9A;}";
@@ -36,3 +36,47 @@ else {
 }
 
 document.getElementsByTagName("head")[0].appendChild( style );
+
+(function() {var css = [
+	"@namespace html url(http://www.w3.org/1999/xhtml);",
+	"",
+	"*",
+	"{",
+	"    font-family: \"Open Sans\", \"Microsoft YaHei\", sans-serif;",
+	"}",
+	"",
+	"h1, h2, h3, h4, h5, h6, h1 *, h2 *, h3 *, h4 *, h5 *, h6 *",
+	"{",
+	"    font-family: \"Open Sans\", \"Roboto\", sans-serif;",
+	"    text-rendering: optimizeLegibility;",
+	"}",
+	"",
+	"h1, h1 *",
+	"{",
+	"    letter-spacing: -2px;",
+	"}",
+	"",
+	"h2, h2 *",
+	"{",
+	"    letter-spacing: -1px;",
+	"}"
+].join("\n");
+if (typeof GM_addStyle != "undefined") {
+	GM_addStyle(css);
+} else if (typeof PRO_addStyle != "undefined") {
+	PRO_addStyle(css);
+} else if (typeof addStyle != "undefined") {
+	addStyle(css);
+} else {
+	var node = document.createElement("style");
+	node.type = "text/css";
+	node.appendChild(document.createTextNode(css));
+	var heads = document.getElementsByTagName("head");
+	if (heads.length > 0) {
+		heads[0].appendChild(node); 
+	} else {
+		// no head yet, stick it whereever
+		document.documentElement.appendChild(node);
+	}
+}
+})();
