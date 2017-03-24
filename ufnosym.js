37d6cd6ac918618cdaf1d70816f3ea8a5e7915e9
Tom Tan <totan@microsoft.com>
2017-03-24 14:01:58 -0700

    Support call to import

--- ufnosym.js
+++ ufnosym.js
@@ -47,22 +47,58 @@ function ufnosym(address)
     {
         let lineArr = line.split(/\s+/);
         let processed = false;
+        let tmpAddr;
 
-        if (lineArr.length >= 7 && lineArr[2] === 'call'
-            && lineArr[3] === 'dword' && lineArr[4] === 'ptr'
-            && lineArr[6].length > 9 && lineArr[6][0] === '(')
+        if (lineArr.length >= 3 && lineArr[2] === 'call')
         {
-            let importAddr = lineArr[6].slice(1, 9);
-            let dpsOut = ctl.ExecuteCommand('dps ' + importAddr + ' l1');
-            if (dpsOut.Count() == 1)
+            if(lineArr.length >= 7 
+               && lineArr[3] === 'dword' && lineArr[4] === 'ptr'
+               && lineArr[6].length > 9 && lineArr[6][0] === '(')
             {
-                let dpsLine = dpsOut.First();
-                let dpsArr = dpsLine.split(/\s+/);
-                if(dpsArr.length >= 3)
+                let importAddr = lineArr[6].slice(1, 9);
+                let dpsOut = ctl.ExecuteCommand('dps ' + importAddr + ' l1');
+                if (dpsOut.Count() == 1)
                 {
-                    let newLine = line.slice(0, line.indexOf('[') + 1) + dpsArr[2] + ']';
-                    log(newLine);
-                    processed = true;
+                    let dpsLine = dpsOut.First();
+                    let dpsArr = dpsLine.split(/\s+/);
+                    if(dpsArr.length >= 3)
+                    {
+                        let newLine = line.slice(0, line.indexOf('[') + 1) + dpsArr[2] + ']';
+                        log(newLine);
+                        processed = true;
+                    }
+                }
+            }
+            else if (lineArr.length == 5 && (tmpAddr = lineArr[4].match(/\(([\da-f`]+)\)/)))
+            {
+                let callAddr = tmpAddr[1];
+                let callTargetDis = ctl.ExecuteCommand('u ' + callAddr + ' l1');
+                if (callTargetDis.Count() == 2)
+                {
+                    let i = 0;
+                    for (let callTargetLine of callTargetDis)
+                    {
+                        if (i++ === 0) continue;
+                        let callTargetArr = callTargetLine.split(/\s+/);
+                        if (callTargetArr.length === 7 && callTargetArr[2] === 'jmp'
+                            && callTargetArr[3] === 'dword' && callTargetArr[4] === 'ptr'
+                            && callTargetArr[6].length > 9 && callTargetArr[6][0] === '(')
+                        {
+                            let importAddr = callTargetArr[6].slice(1, 9);
+                            let dpsOut = ctl.ExecuteCommand('dps ' + importAddr + ' l1');
+                            if (dpsOut.Count() == 1)
+                            {
+                                let dpsLine = dpsOut.First();
+                                let dpsArr = dpsLine.split(/\s+/);
+                                if(dpsArr.length >= 3)
+                                {
+                                    let newLine = line.replace(lineArr[3], '_imp_' + dpsArr[2]);
+                                    log(newLine);
+                                    processed = true;
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
