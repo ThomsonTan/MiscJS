e6757de6982fbd5e1b60cb406e189bbad2c2cfc0
Thomson <lilotom@gmail.com>
2020-08-08 18:14:37 -0700

    Detect complext GitHub pullrequest URL pattern

--- CCE/background.js
+++ CCE/background.js
@@ -4,18 +4,26 @@ chrome.contextMenus.create({
     "onclick": function(e){
         var url = e.pageUrl;
         if (e.selectionText) {
-            
+            url = e.selectionText;
+        } else if (e.linkUrl) {
+            url = e.linkUrl;
         }
-        if (e.linkUrl && e.linkUrl.startsWith('https://github.com/') && e.linkUrl.indexOf('pull') != -1) {
-            var newUrl = 'codeflow:open?pullrequest=' + e.linkUrl + '&ref=ChromeExtension';
-            // chrome.tabs.create({"url": newUrl});
-            // use window.location, no new tab since this url will not populate current window
-            chrome.tabs.query(
-                {currentWindow: true, active: true}, 
-                function(tab) {
-                    chrome.tabs.update(tab.id, {url:newUrl});
-                }
-            );
+
+        if (url) {
+            let prUrlRe = /^https:\/\/github\.com\/[^\/]+\/[^\/]+\/pull\/\d+/i;
+            let urlMatch = url.match(prUrlRe);
+            if (urlMatch && urlMatch.length === 1) {
+                url = urlMatch[0];
+                var newUrl = 'codeflow:open?pullrequest=' + url + '&ref=ChromeExtension';
+                // chrome.tabs.create({"url": newUrl});
+                // use window.location, no new tab since this url will not populate current window
+                chrome.tabs.query(
+                    {currentWindow: true, active: true}, 
+                    function(tab) {
+                        chrome.tabs.update(tab.id, {url:newUrl});
+                    }
+                );
+            }
         }
     }
 });
