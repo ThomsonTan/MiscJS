0ed89dc0281ea9761603eb43c81de100350a2234
Tom Tan <Tom.Tan@microsoft.com>
2021-01-24 18:15:11 -0800

    Hook github commit to customized protocol

--- CCE/background.js
+++ CCE/background.js
@@ -14,20 +14,34 @@ chrome.contextMenus.create({
             let urlMatch = url.match(prUrlRe);
             if (urlMatch && urlMatch.length === 1) {
                 url = urlMatch[0];
-                var newUrl = 'codeflow:open?pullrequest=' + url + '&ref=ChromeExtension';
+                let newUrl = 'codeflow:open?pullrequest=' + url + '&ref=ChromeExtension';
                 // chrome.tabs.create({"url": newUrl});
                 // use window.location, no new tab since this url will not populate current window
                 chrome.tabs.query(
                     {currentWindow: true, active: true}, 
                     function(tab) {
                         chrome.tabs.update(tab.id, {url:newUrl});
-                        if (typeof(tab[0].url) === 'string') {
-                            let currentPageUrl = tab[0].url;
-                            let currentPageUrlMatch = currentPageUrl.match(prUrlRe);
-                            if (currentPageUrlMatch && currentPageUrlMatch.length === 1) {
-                                window.setTimeout(function() {chrome.tabs.remove(tab[0].id, function(){});}, 8000);
-                            }
-                        }
+                        // if (typeof(tab[0].url) === 'string') {
+                        //     let currentPageUrl = tab[0].url;
+                        //     let currentPageUrlMatch = currentPageUrl.match(prUrlRe);
+                        //     if (currentPageUrlMatch && currentPageUrlMatch.length === 1) {
+                        //         window.setTimeout(function() {chrome.tabs.remove(tab[0].id, function(){});}, 8000);
+                        //     }
+                        // }
+                    }
+                );
+
+                return ;
+            }
+
+            let commitUrlRe = /^https:\/\/github\.com\/[^\/]+\/[^\/]+\/commit\/[^\/]+/i;
+            let commitMatch = url.match(commitUrlRe);
+            if (commitMatch && commitMatch.length === 1) {
+                let newUrl = 'pythonprotocol:open?commit=' + url;
+                chrome.tabs.query(
+                    {currentWindow: true, active: true}, 
+                    function(tab) {
+                        chrome.tabs.update(tab.id, {url:newUrl});
                     }
                 );
             }
